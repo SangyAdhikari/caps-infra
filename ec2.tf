53a6f0a882fb5f31cfb75b1d246425d157b55989
SangyAdhikari <105117035+SangyAdhikari@users.noreply.github.com>
2022-09-21 12:15:20 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -32,3 +32,42 @@ resource "aws_instance" "instance_type1" {
     Name = "stagging"
   }
 }
+
+resource "aws_eip" "lb" {
+  instance = aws_instance.instance_type.id
+  vpc      = true
+}
+
+resource "aws_instance" "instance_type2" {
+  ami                         = "ami-0568773882d492fc8"
+  instance_type               = var.instance_type
+  vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
+  subnet_id                   = aws_subnet.subnet1-public.id
+  associate_public_ip_address = true
+  availability_zone           = var.azs
+  #user_data                   = file("docker.sh")
+  #user_data                   = "${file("jenkins.sh")}"
+  key_name                    = var.key_name
+  tags = {
+    Name = "kubernetes-master"
+  }
+}
+
+resource "aws_instance" "instance_type3" {
+  ami                         = "ami-0568773882d492fc8"
+  instance_type               =  var.instance_type1
+  vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
+  subnet_id                   = aws_subnet.subnet1-public.id
+  associate_public_ip_address = true
+  availability_zone           = var.azs
+  #user_data                   = file("docker.sh")
+  #user_data                   = "${file("jenkins.sh")}"
+  key_name                    = var.key_name
+  tags = {
+    Name = "kubernetes-workernode1"
+  }
+}
+
+
+
+
