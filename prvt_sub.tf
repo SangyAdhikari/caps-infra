a417e56edf33305f7a35faa9eabcdcfae9099b8f
SangyAdhikari <105117035+SangyAdhikari@users.noreply.github.com>
2022-09-21 10:48:48 +0530

    Update prvt_sub.tf

--- prvt_sub.tf
+++ prvt_sub.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "subnet1-private" {
   vpc_id            = aws_vpc.default.id
   cidr_block        = var.private_subnet1_cidr
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-2b"
 
   tags = {
     Name = "${var.private_subnet1_name}"
